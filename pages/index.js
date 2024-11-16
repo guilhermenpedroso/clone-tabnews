7f3eb6667b780ce3cc8de73332033f940e582551
Guilherme Pedroso <guilhermenpedroso@outlook.com>
2024-11-16 17:55:17 -0300

    Mudando alguns arquivos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home(){
-    return <h1>Teste 2</h1>
+    return <h1>
+        Você prefere empanadas num restaurante chileno ou pad thai num restaurante vietnamita?
+        Eu já conferi e os dois tem opções vegetarianas :))
+    </h1>
 }
 
 export default Home

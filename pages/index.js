75150f6ccf2c6a5b37f6668ad5c9e336eeb18774
Guilherme Pedroso <guilhermenpedroso@outlook.com>
2024-11-18 14:20:43 -0300

    atualizando a mensagem novamente

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <p>
-        esquecemos de combinar o dia e horário pra irmos comer um pad thai, pensei em jantarmos na semana, que tal? Final de semana eu vou viajar.
+        tô aprendendo a fazer programa
     </p>
 }
 
